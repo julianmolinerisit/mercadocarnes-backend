1097fa1b836db117b59888b91b8300c77785cd3c
Julian Molineris <110922176+julianmolinerisit@users.noreply.github.com>
2023-09-28 20:11:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import dbConnect from './util/mongo.js';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
